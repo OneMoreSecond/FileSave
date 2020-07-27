b49dc3ff27fcd1bfba2cecd65f525631fb85e1f8
Tang Xianghao <t-x.h@163.com>
2020-07-27 19:03:50 +0800

    Support in-page preview

--- javlib.js
+++ javlib.js
@@ -6,7 +6,7 @@
 // @grant       none
 // ==/UserScript==allow pasting
 
-let jav_prefix_dict = {
+let video_id_prefix_dict = {
     'aby': '118',
     'abp': '118',
     'abs': '118',
@@ -24,37 +24,62 @@ let jav_prefix_dict = {
     'fsdss': '1',
     'msfh': '1',
     'gvh': '13',
-};
+}
 
-let div = document.getElementById('video_id');
-//alert(div.innerHTML);
-let row = div.getElementsByTagName('tr')[0];
-//alert(row.innerHTML);
-let jav_id = row.cells[1].textContent;
-//alert(jav_id);
+let video_id_div = document.getElementById('video_id')
+let video_id_row = video_id_div.getElementsByTagName('tr')[0]
+let video_id = video_id_row.cells[1].textContent
 
-var jav_prefix = jav_id.slice(0, -4).toLowerCase();
-if(jav_prefix in jav_prefix_dict)
+var video_id_prefix = video_id.slice(0, -4).toLowerCase()
+if(video_id_prefix in video_id_prefix_dict)
 {
-    jav_prefix = jav_prefix_dict[jav_prefix] + jav_prefix;
+    video_id_prefix = video_id_prefix_dict[video_id_prefix] + video_id_prefix
 }
 else
 {
-    jav_prefix += '00'
+    video_id_prefix += '00'
 }
 
-let jav_number = jav_id.slice(-3);
+let video_id_suffix = video_id.slice(-3)
 
-let video_id = jav_prefix + jav_number;
+let dmm_id = video_id_prefix + video_id_suffix
 //alert(video_id);
 
 function addLink(version)
 {
-    let url = `http://cc3001.dmm.co.jp/litevideo/freepv/${video_id[0]}/${video_id.substring(0,3)}/${video_id}/${video_id}_${version}_w.mp4`
-    var cell = row.insertCell(-1);
-    cell.innerHTML = '<a href="' + url + '">' + version + '</a>';
+    let url = `http://cc3001.dmm.co.jp/litevideo/freepv/${dmm_id[0]}/${dmm_id.substring(0,3)}/${dmm_id}/${dmm_id}_${version}_w.mp4`
+    var cell = video_id_row.insertCell()
+    let link_tag = document.createElement('a')
+    link_tag.href = url
+    link_tag.innerHTML = version
+    cell.appendChild(link_tag)
+    return url
 }
 
-addLink('dmb');
-addLink('dm');
-addLink('sm');
+let urls = []
+for (version of ['dmb', 'dm', 'sm'])
+{
+    urls.push(addLink(version))
+}
+
+for (url of urls)
+{
+    let http = new XMLHttpRequest()
+    http.open('HEAD', url, false)
+    http.send();
+    if (http.status != 404)
+    {
+        let video_tag = document.createElement('video')
+        video_tag.src = url
+        video_tag.controls = true
+
+        let preview_thumbs = document.getElementsByClassName('previewthumbs')[0]
+        right_column_div.removeChild(preview_thumbs)
+
+        let preview_row = document.getElementById('video_jacket_info').insertRow()
+        preview_row.insertCell().appendChild(video_tag)
+        preview_row.insertCell().appendChild(preview_thumbs)
+
+        break;
+    }
+}
