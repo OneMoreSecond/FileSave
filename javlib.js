d170d373ba91975d263e960eaba1692c2d3719a0
Tang Xianghao <t-x.h@163.com>
2020-07-27 15:23:00 +0800

    Update code about address string

--- javlib.js
+++ javlib.js
@@ -6,7 +6,7 @@
 // @grant       none
 // ==/UserScript==allow pasting
 
-var jav_prefix_dict = {
+let jav_prefix_dict = {
     'aby': '118',
     'abp': '118',
     'abs': '118',
@@ -26,11 +26,11 @@ var jav_prefix_dict = {
     'gvh': '13',
 };
 
-var div = document.getElementById('video_id');
+let div = document.getElementById('video_id');
 //alert(div.innerHTML);
-var row = div.getElementsByTagName('tr')[0];
+let row = div.getElementsByTagName('tr')[0];
 //alert(row.innerHTML);
-var jav_id = row.cells[1].textContent;
+let jav_id = row.cells[1].textContent;
 //alert(jav_id);
 
 var jav_prefix = jav_id.slice(0, -4).toLowerCase();
@@ -43,22 +43,16 @@ else
     jav_prefix += '00'
 }
 
-var jav_number = jav_id.slice(-3);
+let jav_number = jav_id.slice(-3);
 
-var video_id = jav_prefix + jav_number;
+let video_id = jav_prefix + jav_number;
 //alert(video_id);
 
-var urlPrefix = 'http://cc3001.dmm.co.jp/litevideo/freepv/'
-    + jav_prefix[0] + '/'
-    + jav_prefix.substring(0,3) + '/'
-    + video_id + '/'
-    + video_id + '_';
-
-function addLink(prefix)
+function addLink(version)
 {
-    var url = urlPrefix + prefix + '_w.mp4';
+    let url = `http://cc3001.dmm.co.jp/litevideo/freepv/${video_id[0]}/${video_id.substring(0,3)}/${video_id}/${video_id}_${version}_w.mp4`
     var cell = row.insertCell(-1);
-    javcell.innerHTML = '<a href="' + url + '">' + prefix + '</a>';
+    cell.innerHTML = '<a href="' + url + '">' + version + '</a>';
 }
 
 addLink('dmb');
