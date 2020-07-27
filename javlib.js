4aa8136efd71e7b0552e098d70cd3d024b8ef1c5
Tang Xianghao <t-x.h@163.com>
2020-07-27 15:17:57 +0800

    Format code

--- javlib.js
+++ javlib.js
@@ -5,40 +5,60 @@
 // @version     1
 // @grant       none
 // ==/UserScript==allow pasting
-//alert('hello');
-var idPrefixDict = {'aby':'118aby', 'abp':'118abp', 'abs':'118abs', 'star':'1star','stars':'1stars', 'ppt':'118ppt',  'tem':'118tem', 'bgn':'118bgn','mgt':'118mgt','dtt':'118dtt', 'kbi':'118kbi', 'chn':'118chn', 'sdnm':'1sdnm', 'fsdss':'1fsdss', 'gvh':'13gvh', 'msfh':'1msfh', 'dic':'118dic'};
-//alert('hello');
+
+var jav_prefix_dict = {
+    'aby': '118',
+    'abp': '118',
+    'abs': '118',
+    'dic': '118',
+    'ppt': '118',
+    'tem': '118',
+    'bgn': '118',
+    'mgt': '118',
+    'dtt': '118',
+    'kbi': '118',
+    'chn': '118',
+    'star': '1',
+    'stars': '1',
+    'sdnm': '1',
+    'fsdss': '1',
+    'msfh': '1',
+    'gvh': '13',
+};
+
 var div = document.getElementById('video_id');
 //alert(div.innerHTML);
 var row = div.getElementsByTagName('tr')[0];
 //alert(row.innerHTML);
-var id = row.cells[1].textContent;
-//alert(id);
-var idPrefix = id.slice(0, -4).toLowerCase();
-if(idPrefix in idPrefixDict)
+var jav_id = row.cells[1].textContent;
+//alert(jav_id);
+
+var jav_prefix = jav_id.slice(0, -4).toLowerCase();
+if(jav_prefix in jav_prefix_dict)
 {
-  idPrefix = idPrefixDict[idPrefix];
+    jav_prefix = jav_prefix_dict[jav_prefix] + jav_prefix;
 }
 else
 {
-  idPrefix += '00'
+    jav_prefix += '00'
 }
-var idSuffix = id.slice(-3);
 
-var newId = idPrefix + idSuffix;
-//alert(newId);
+var jav_number = jav_id.slice(-3);
+
+var video_id = jav_prefix + jav_number;
+//alert(video_id);
 
 var urlPrefix = 'http://cc3001.dmm.co.jp/litevideo/freepv/'
-+ idPrefix[0] + '/'
-+ idPrefix.substring(0,3) + '/'
-+ newId + '/'
-+ newId + '_';
+    + jav_prefix[0] + '/'
+    + jav_prefix.substring(0,3) + '/'
+    + video_id + '/'
+    + video_id + '_';
 
 function addLink(prefix)
 {
-  var url = urlPrefix + prefix + '_w.mp4';
-  var cell = row.insertCell(-1);
-  cell.innerHTML = '<a href="' + url + '">' + prefix + '</a>';
+    var url = urlPrefix + prefix + '_w.mp4';
+    var cell = row.insertCell(-1);
+    javcell.innerHTML = '<a href="' + url + '">' + prefix + '</a>';
 }
 
 addLink('dmb');
